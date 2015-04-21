0b58773cdebab58885ca20f221eb3b27c0cd83d1
Erik <xxxsmoizxxx@gmail.com>
2015-04-21 21:54:30 +0200

    Added gravatar and removed fontawesome

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -2,3 +2,11 @@ Template.registerHelper('truncate', function(string, length) {
   var cleanString = _(string).stripTags();
   return _(cleanString).truncate(length);
 });
+
+Template.registerHelper('avatar_url', function(email, size) {
+  return Gravatar.imageUrl(email, {
+    'size': size,
+    default: 'mm',
+    secure:true,
+  });
+});
